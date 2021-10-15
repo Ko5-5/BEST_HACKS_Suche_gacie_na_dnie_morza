a9f999b88d690848176ba32cfba47b76fd96fe80
kczaplicki <jakub.czaplicki@applover.pl>
2021-10-15 23:19:01 +0200

    first commit

---
+++ save_me/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(SaveMe());
+
+
+  class SaveMe extends StatelessWidget {
+    @override
+    Widget build(BuildContext context) {
+      return Container();
+    }
+  }
